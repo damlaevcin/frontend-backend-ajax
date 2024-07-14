3318c478adf286c960cd8ae624627730540fd36e
damlaevcin <dmlaevcinn@gmail.com>
2024-07-14 18:38:25 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 $(document).on("click", ".get-info-btn", function () {
     $.ajax({
-        url: "https://jsonplaceholder.typicode.com/posts/1",
+        url: "https://jsonplaceholder.typicode.com/posts",
         type: "GET",
         dataType: "json",
         success: function (data) {
-            $(".id").text(data.id);
-            $(".userid").text(data.userId);
-            $(".title").text(data.title);
-            $(".body").text(data.body);
+            var newsTitleHtml = "";
+            data.forEach(function(value, key){
+                newsTitleHtml = newsTitleHtml + '<div class="row w-100 mt-5"><button class="btn btn-dark w-100">'+ value.title +'</button></div>'
+            })
+            $(".news-titles").html(newsTitleHtml)
         },
         error: function (error) {
             console.log(error)
